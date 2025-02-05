327de017f258ee8de0c938a2c5c3eb4789bd5dd1
Catarina Cepeda <catarinacepeda@tecnico.ulisboa.pt>
2025-02-05 10:21:50 +0000

    Add basic functionality: generate publication and summary

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { enhance } from '$app/forms';
+  import { goto } from '$app/navigation';
+  const { data } = $props();
+
+  const minTopics = 2,
+    maxTopics = 5;
+
+  /** @type {number[]} */
+  let selectedTopics = $state([]);
+  let generating = $state(false);
+
+  function onGenerate() {
+    generating = true;
+    return async ({ update, action }) => {
+      generating = false;
+      await goto(action);
+      await update();
+    };
+  }
+</script>
+
+<h1>Postdigital Publishing Practices: on Hybrid and Processual Print</h1>
+<p>Select {minTopics} to {maxTopics} topics to generate a publication</p>
+
+<form method="POST" action="/publication" use:enhance={onGenerate}>
+  <div>
+    {#each data.topics as topic}
+      <label>
+        <input type="checkbox" name="topic" value={topic.id} bind:group={selectedTopics} />
+        {topic.terms[0]}
+      </label>
+    {/each}
+  </div>
+  <div>
+    <button type="submit">Generate publication</button>
+  </div>
+</form>
+
+{#if generating}
+  <p>generating...</p>
+{/if}
---
+++ src/routes/publication/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  /** @type {import('./$types').PageProps} */
+  let { form } = $props();
+</script>
+
+<h1>{form?.title}</h1>
+<h2>Summary</h2>
+<p>{form?.summary}</p>
+<p>Topics: {form?.topics.join(', ')}</p>
+<hr>
+{#each form?.docs as doc}
+<p>{doc.Document}</p>
+{/each}
